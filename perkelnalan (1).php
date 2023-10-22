b1728994ad51902d0a810e7f7697e9c89da6d195
AliefMardiansyah <148052521+AliefMardiansyah@users.noreply.github.com>
2023-10-22 14:52:27 +0700

    Tugas 1

---
+++ perkelnalan (1).php
@@ -0,0 +1,14 @@
+<?php
+
+$nim = "701220314";
+$nama = "Alief Mardiansyah";
+$umur = 19;
+$nilai = 75.75;
+
+echo "Nim   : ". $nim . "</br>";
+echo "Nama  : $nama</br>";
+print "umur  : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai : %.3f</br>", $nilai);
+
+?>
